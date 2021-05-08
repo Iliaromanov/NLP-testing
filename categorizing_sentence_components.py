3e381e7b69784e2cbc751d93e6d02ac7c8c944d3
Iliaromanov <iliaromanov033@gmail.com>
2021-05-08 15:49:00 -0400

    categorizing sentence components

---
+++ categorizing_sentence_components.py
@@ -0,0 +1,6 @@
+###########################################################
+# Goal: parse block of text and extract categorical data  #
+#       eg: "Address 123 Lester, Suite 567" =>            #
+#             {'Address': "123 Lester", 'Suite': "567"}   #
+###########################################################
+
